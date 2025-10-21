5abcf8eddc43a03e3dbacd9c13da45e6f3edd39a
Merouane06110 <merouane.zina@oclock.school>
2025-10-21 13:24:58 +0200

    Routes-gestion-portfolio : mise à jour des modèles, associations et contrôleur portfolio

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,21 +1,19 @@
-import express from "express";
-import companyRoute from "./companyRoute.js";
-import userRoute from "./userRoute.js";
 import express from 'express';
 import companyRoute from './companyRoute.js';
+import userRoute from './userRoute.js';
 import messageContactRoute from './messageContactRoute.js';
 import previewRoute from './previewRoute.js';
 import genreRoute from './genreRouter.js';
+import portfolioRoute from './portfolioRoute.js'; // <-- notre nouvelle route portfolio
 
 const router = express.Router();
 
-router.use("/api", companyRoute);
-router.use("/api", userRoute);
-
-export default router;
+// Toutes les routes API
 router.use('/api', companyRoute);
+router.use('/api', userRoute);
 router.use('/api', messageContactRoute);
 router.use('/api', previewRoute);
-router.use('/api', genreRoute );
+router.use('/api', genreRoute);
+router.use('/api', portfolioRoute); // <-- ajout du portfolio
 
 export default router;
